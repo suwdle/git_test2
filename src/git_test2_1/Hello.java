bcd16ac7e3e7b771d6ae11ee5a4374d27720e3a9
suwdle <suwdle1917@gmail.com>
2024-09-11 11:22:40 +0900

    화면 출력 방법 수정

--- src/git_test2_1/Hello.java
+++ src/git_test2_1/Hello.java
@@ -3,8 +3,8 @@ package git_test2_1;
 public class Hello {
 	
 	String message = "안녕하세요";
-	public void say() {
-		System.out.println(message);
+	public String say() {
+		return message;
 	}
 
 }
--- src/git_test2_1/Main.java
+++ src/git_test2_1/Main.java
@@ -3,7 +3,7 @@ package git_test2_1;
 public class Main {
 	public static void main(String[] args) {
 		Hello hello = new Hello();
-		hello.say();
+		System.out.println(hello.say());
 	}
 
 }
